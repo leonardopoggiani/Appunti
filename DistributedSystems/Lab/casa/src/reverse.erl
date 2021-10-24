d319f5899fca1fe93e7048ffc13c651ffbb17e9e
Leonardo Poggiani <leonardo.poggiani@gmail.com>
2021-10-24 10:51:44 +0200

    es

--- DistributedSystems/Lab/casa/src/reverse.erl
+++ DistributedSystems/Lab/casa/src/reverse.erl
@@ -10,17 +10,17 @@
 -author("leonardopoggiani").
 
 %% API
--export([tail_reverse/1]).
+-export([reverse/1]).
 
 %%%% ESERCIZIO 6 %%%%
 
 % implement the function reverse that bring a list and print it reversed.
 
-tail_reverse(L) ->
-  tail_reverse(L,[]).
+reverse(L) ->
+  reverse(L,[]).
 
-tail_reverse([],Acc) ->
+reverse([],Acc) ->
   Acc;
 
-tail_reverse([H|T],Acc) ->
-  tail_reverse(T, [H|Acc]).
+reverse([H|T],Acc) ->
+  reverse(T, [H|Acc]).
